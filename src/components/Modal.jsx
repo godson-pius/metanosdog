b07b88c41d680a9e74dabd4bfec54cdba987ce0a
Godson Pius <40927597+godson-pius@users.noreply.github.com>
2024-06-05 07:34:25 +0100

    Added background color to the modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,7 +4,7 @@ const Modal = ({ title, modal, setModal, showclose = true, moreWidth = false, ch
     return (
         <main>
             <dialog id="my_modal_3" className={`modal ${modal && 'modal-open'} w-full`} style={{ width: '100%' }}>
-                <div className={`modal-box ${moreWidth ? 'w-11/12 max-w-5xl' : null}`}>
+                <div className={`modal-box bg-white ${moreWidth ? 'w-11/12 max-w-5xl' : null}`}>
                     {showclose ? (<button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={() => setModal(false)}>✕</button>) : null}
                     <h3 className="font-bold text-lg">{title}</h3>
                     <div className="pt-4">{children}</div>
